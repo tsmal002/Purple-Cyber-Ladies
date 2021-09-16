676cb0b6c63d5bddc90d01660306d2a20886cab9
tsmal002 <81273905+tsmal002@users.noreply.github.com>
2021-09-16 18:42:17 -0400

    Add files via upload

--- search-report-result.php
+++ search-report-result.php
@@ -1,9 +1,70 @@
+<?php session_start();
+//DB conncetion
+include_once('includes/config.php');
+//error_reporting(0);
+//validating Session
+if (strlen($_SESSION['aid']==0)) {
+  header('location:logout.php');
+  } else{
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Covid-Tms | Search Report</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+  <?php include_once('includes/sidebar.php');?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+<?php include_once('includes/topbar.php');?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
 <?php
-$searchdata=$_POST['serachdata'];
+$searchdata=$_POST['searchdata'];
+$san_searchdata = filter_var($searchdata, FILTER_SANITIZE_ADD_SLASHES)
+ )
 
 ?>
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Search Result Againt '<?php echo $searchdata;?>' </h1>
+                    <h1 class="h3 mb-2 text-gray-800">Search Result Againt '<?php echo $san_searchdata;?>' </h1>
     
 
                     <!-- DataTales Example -->
@@ -40,9 +101,9 @@ $searchdata=$_POST['serachdata'];
                                         </tr>
                                     </tfoot>
                                     <tbody>
-<?php $query=mysqli_query($stmt = $conn-> prepare("INSERT INTO (tbltestrecord.OrderNumber,tblpatients.FullName,tblpatients.MobileNumber,tbltestrecord.TestType,tbltestrecord.TestTimeSlot,tbltestrecord.RegistrationDate,tbltestrecord.id as testid from tbltestrecord
-join tblpatients on tblpatients.MobileNumber=tbltestrecord.PatientMobileNumber)");
-$stmt->bind_param("sss", $tblpatients.FullName, $tblpatients.MobileNumber, $tbltestrecord.OrderNumber);
+<?php $query=mysqli_query($con,"select tbltestrecord.OrderNumber,tblpatients.FullName,tblpatients.MobileNumber,tbltestrecord.TestType,tbltestrecord.TestTimeSlot,tbltestrecord.RegistrationDate,tbltestrecord.id as testid from tbltestrecord
+join tblpatients on tblpatients.MobileNumber=tbltestrecord.PatientMobileNumber
+where (tblpatients.FullName like '%$san_searchdata%' || tblpatients.MobileNumber like '%$san_searchdata%' || tbltestrecord.OrderNumber like '%$san_searchdata%')
 $cnt=1;
 while($row=mysqli_fetch_array($query)){
 ?>
@@ -64,3 +125,47 @@ while($row=mysqli_fetch_array($query)){
 <?php $cnt++;} ?>
                                     </tbody>
                                 </table>
+                                     </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+
+            <!-- Footer -->
+    <?php include_once('includes/footer.php');?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <?php include_once('includes/footer2.php');?>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+</body>
+</html>
+<?php } ?>
